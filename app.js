52a70ec29001cca4718d5efeb3dcb0fb21d20306
candenunez <candeenunez@gmail.com>
2022-02-07 21:49:36 -0300

    heroku

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
