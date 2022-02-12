dbe326a636fd7c89fef1f4a3fdcf8c8419793b6f
DanielFranco <dfranco@neu.com.co>
2022-02-12 12:27:05 -0500

    "Minor changes"

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ app.use(express.static('public'));
 
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
+    console.log("Server running");
 })
+/**
+app.listen(3000, ()=>{
+    console.log('Server running');
+});
+ */
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
-
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
